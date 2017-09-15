8c6403c2c4a03af5db95f79bd7fd218fd174d84f
pcmkv <maxwell1111@mail.ru>
2017-09-15 12:45:52 +0300

    test

--- src/Table.js
+++ src/Table.js
@@ -9,35 +9,36 @@ class Table extends Component {
     super(props);
     console.log("CHILDREN",this.props.data);
   }
-  
+
   render() {
     return (
       <div className="row">
         <div className="col-sm-12">
-          <table className="table table-bordered">
+            <table className="table table-bordered">
             <thead>
             <tr>
-              <th>#</th>
-              <th>name</th>
-              <th>E-mail</th>
-              <th>Location</th>
+            <th>#</th>
+            <th>name</th>
+            <th>E-mail</th>
+            <th>Location</th>
             </tr>
             </thead>
             <tbody>
             {
-              this.props.data.map((elem,i)=>{
-                return(
-                    <tr key={i} className={i}>
-                      <td>{elem.id} </td>
-                      <td>{elem.email} </td>
-                      <td>{elem.name} </td>
-                      <td> </td>
-                    </tr>
-                )}
+              this.props.data.map((elem, i)=> {
+                    return (
+                        <tr key={i} className={i}>
+                          <td>{elem.id} </td>
+                          <td>{elem.email} </td>
+                          <td>{elem.name} </td>
+                          <td></td>
+                        </tr>
+                    )
+                  }
               )
-              }
+            }
             </tbody>
-          </table>
+            </table>
         </div>
       </div>
     );
